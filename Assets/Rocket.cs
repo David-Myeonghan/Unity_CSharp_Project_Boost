d196ba8826d46e016e6e8c9a2a362828e5e5711c
David MH Ryu <myeonghan12@gmail.com>
2019-01-29 23:53:33 +1000

    tag

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcsThrust = 100f; // sensable default
+    [SerializeField] float mainThrust = 100f;
 
     Rigidbody rigidbody;
     AudioSource audioSource;
@@ -14,20 +16,38 @@ public class Rocket : MonoBehaviour
     {
         rigidbody = GetComponent<Rigidbody>(); // 'Generics'
         audioSource = GetComponent<AudioSource>();
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag) 
+        {
+            case "Friendly":
+                print("OK"); // todo remove
+                break;
+            case "Fuel":
+                print("Fuel");
+                break;
+            default:
+                print("Dead");
+                break;
+        }
+    }
+
+
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
-            rigidbody.AddRelativeForce(Vector3.up);
+            rigidbody.AddRelativeForce(Vector3.up * mainThrust);
+
             if (!audioSource.isPlaying) // so it doesn't layer
             {
                 audioSource.Play();
@@ -37,16 +57,26 @@ public class Rocket : MonoBehaviour
         {
             audioSource.Stop();
         }
+    }
 
+    private void Rotate()
+    {
+        rigidbody.freezeRotation = true; // take manual control of roation
 
-        if (Input.GetKey(KeyCode.A)) 
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward); // anti-clockwise // left-handed system
+            transform.Rotate(Vector3.forward * rotationThisFrame); // anti-clockwise // left-handed system
         }
 
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
+
+        rigidbody.freezeRotation = false; // resume physics control of rotation
     }
+
+
 }
